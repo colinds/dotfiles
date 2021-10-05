bc3e3537c1bdd98ef0c3ae968e480cc92838fb1f
Colin D'Souza <colin.dsouza@shopify.com>
2021-10-05 10:18:55 -0400

    update

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
-sh -c "$(curl -fsSL https://starship.rs/install.sh)"
+sh -c "$(curl -fsSL https://starship.rs/install.sh)";
 
 ln -sf ~/dotfiles/zshrc ~/.zshrc;
+
+mkdir -p ~/.config;
+ln -sf ~/dotfiles/starship.toml ~/.config/starship.toml;
